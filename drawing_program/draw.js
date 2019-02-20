b02aa62dc30043e1f42b8d23dbb233c26f4ac1cb
Orion Riitters <orionriitters@gmail.com>
2019-02-20 17:32:31 -0600

    Finished draw_program.

--- drawing_program/draw.js
+++ drawing_program/draw.js
@@ -1,15 +1,57 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let slider = document.getElementById('brush-size');
+let sliderLabel = document.getElementById('brush-label');
+let brushColor = document.getElementById('color-input');
+let brushCircle = document.getElementById('brush-circle');
+let brushSquare = document.getElementById('brush-square');
+let isMouseDown = false;
 
-canvas.addEventListener('mousedown', draw());
-canvas.addEventListener('mousemove', draw());
+let brush = {
+    radius: 10,
+    strokeType: 'square',
+    color: '#000000'
+};
 
+canvas.addEventListener('mousedown', () => {
+    isMouseDown = true;
+    draw();
+});
+canvas.addEventListener('mousemove', () => {
+    if (isMouseDown) draw();
+});
 
+canvas.addEventListener('mouseup', () => isMouseDown = false);
 
-function draw(brush) {
+slider.oninput = () => {
+    sliderLabel.innerHTML = 'Brush Size: ' + slider.value;
+    brush.radius = slider.value;
+};
+
+brushColor.addEventListener('change', () => {
+    brush.color = brushColor.value;
+    document.getElementById("color-label").innerHTML = 'HEX Color: ' + brushColor.value;
+});
+
+brushCircle.addEventListener('click', function() {
+    brush.strokeType = this.getAttribute('brush');
+});
+
+brushSquare.addEventListener('click', function() {
+    brush.strokeType = this.getAttribute('brush');
+});
+
+function draw() {
     var x = event.offsetX;
     var y = event.offsetY;
-    console.log('asdf');
+    ctx.fillStyle = brush.color;
 
+    if (brush.strokeType !== 'square') {
+        ctx.beginPath();
+        ctx.arc(x, y, brush.radius, 0, 2 * Math.PI);
+        ctx.fill();
+    } else  ctx.fillRect((x - brush.radius / 2), (y - brush.radius / 2), brush.radius, brush.radius);
 
 }
+
+
